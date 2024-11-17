ac8025cd23af42dffbe2525c2c958773f75e0032
Sirsam <samuelpuhakka@gmail.com>
2024-11-17 19:21:26 +0200

    Modified App.js and package.json

---
+++ FutureCastApp/src/backend/routes/predRouter.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getAllPredictions,
+    createPrediction,
+    getPredictionById,
+    getPredictionByUsername,
+    updatePrediction,
+    deletePrediction
+    // patchPrediction
+} = require("../controllers/predController");
+
+// GET /predictions
+router.get("/", getAllPredictions);
+
+// POST /predictions
+router.post("/", createPrediction);
+
+// GET /predictions/:predictionId
+router.get("/:predictionId", getPredictionById);
+
+// GET /predictions/:username
+router.get("/username/:username", getPredictionByUsername);
+
+// PUT /predictions/:predictionId
+router.put("/:predictionId", updatePrediction);
+
+// DELETE /predictions/:predictionId
+router.delete("/:predictionId", deletePrediction);
+
+// Update prediction using PATCH
+// router.patch('/:predictionId', patchPrediction)
+
+module.exports = router;
