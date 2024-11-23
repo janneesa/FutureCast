e39a33926f23c90e324a2405fdf08e4fd85fa274
janneesa <janne.savinainen@hotmail.com>
2024-11-23 19:23:07 +0200

    preference settings works with backend

--- FutureCastApp/frontend/src/components/settingsComponent/PreferenceSettings.jsx
+++ FutureCastApp/frontend/src/components/settingsComponent/PreferenceSettings.jsx
@@ -6,22 +6,46 @@ import Loading from "../Loading";
 import { UserContext } from "../context/UserContext";
 
 function PreferenceSettings() {
-  const { user } = useContext(UserContext);
-  // Load dark mode preference from localStorage
-  const [darkmode, setDarkmode] = useState(() => {
-    const savedPreference = localStorage.getItem("darkmode");
-    return savedPreference ? JSON.parse(savedPreference) : false;
-  });
+  const { user, setUser } = useContext(UserContext);
+  const [darkmode, setDarkmode] = useState(true);
+
+  const [error, setError] = useState("");
+  const [okMessage, setOkMessage] = useState("");
 
   useEffect(() => {
-    if (darkmode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
+    if (user) {
+      setDarkmode(user.settings.preferences.darkMode);
     }
-    // Save dark mode preference to localStorage
-    localStorage.setItem("darkmode", JSON.stringify(darkmode));
-  }, [darkmode]);
+  }, [user]);
+
+  const handleSwitch = async () => {
+    const updatedUser = { ...user };
+    updatedUser.settings.preferences.darkMode = !darkmode;
+
+    try {
+      const response = await fetch(
+        `http://localhost:4000/api/users/${user._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedUser),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data);
+        setOkMessage("Settings updated successfully");
+        setError("");
+      }
+    } catch (error) {
+      setError("Failed to update settings");
+      setOkMessage("");
+      console.error(error);
+    }
+  };
 
   if (!user) {
     return <Loading />;
@@ -36,11 +60,10 @@ function PreferenceSettings() {
       <div className="card-content">
         <div className="flex justify-between items-center">
           <h5>Dark Mode</h5>
-          <Switcher
-            isChecked={darkmode}
-            onChange={() => setDarkmode(!darkmode)}
-          />
+          <Switcher isChecked={darkmode} onChange={handleSwitch} />
         </div>
+        {error && <div className="error-text">{error}</div>}
+        {okMessage && <div className="ok-text">{okMessage}</div>}
       </div>
     </Card>
   );
