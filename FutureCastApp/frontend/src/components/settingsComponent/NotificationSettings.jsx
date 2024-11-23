f9b096abf801b6da62de6bf0e870365b9542e461
janneesa <janne.savinainen@hotmail.com>
2024-11-23 19:08:00 +0200

    Notification settings using backend

--- FutureCastApp/frontend/src/components/settingsComponent/NotificationSettings.jsx
+++ FutureCastApp/frontend/src/components/settingsComponent/NotificationSettings.jsx
@@ -1,18 +1,52 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Card from "../Card";
 import Switcher from "../utility/switcher";
 import Loading from "../Loading";
 import { UserContext } from "../context/UserContext";
 
 function NotificationSettings() {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
   const [emailNotifications, setEmailNotifications] = useState(true);
   const [pushNotifications, setPushNotifications] = useState(true);
 
-  const saveChanges = () => {
-    console.log("Changes saved successfully!");
-    console.log("Email Notifications:", emailNotifications);
-    console.log("Push Notifications:", pushNotifications);
+  const [error, setError] = useState("");
+  const [okMessage, setOkMessage] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      setEmailNotifications(user.settings.notifications.email);
+      setPushNotifications(user.settings.notifications.push);
+    }
+  }, [user]);
+
+  const saveChanges = async () => {
+    const updatedUser = { ...user };
+    updatedUser.settings.notifications.email = emailNotifications;
+    updatedUser.settings.notifications.push = pushNotifications;
+
+    try {
+      const response = await fetch(
+        `http://localhost:4000/api/users/${user._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedUser),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data);
+        setOkMessage("Settings updated successfully");
+        setError("");
+      }
+    } catch (error) {
+      setError("Failed to update settings");
+      setOkMessage("");
+      console.error(error);
+    }
   };
 
   if (!user) {
@@ -40,6 +74,8 @@ function NotificationSettings() {
             onChange={() => setPushNotifications(!pushNotifications)}
           />
         </div>
+        {error && <p className="error-text">{error}</p>}
+        {okMessage && <p className="ok-text">{okMessage}</p>}
         <div className="mt-4 w-32">
           <button className="button" onClick={saveChanges}>
             Save Changes
