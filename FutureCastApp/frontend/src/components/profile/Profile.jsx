83cd358d2ab31e83604a4c55dc466225c87f5f3c
janneesa <janne.savinainen@hotmail.com>
2024-11-26 17:20:48 +0200

    Refactoring profile

--- FutureCastApp/frontend/src/components/profile/Profile.jsx
+++ FutureCastApp/frontend/src/components/profile/Profile.jsx
@@ -1,31 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
-import { mockData } from "../../data/MockData";
-import { useNavigate } from "react-router-dom";
 import ProfileCard from "./ProfileCard";
 import ScoreCard from "./ScoreCard";
 import ContentCard from "./ContentCard";
 import Loading from "../Loading";
 
-function Profile({ profile }) {
+const Profile = ({ profile }) => {
   const { user } = useContext(UserContext);
   const [predictions, setPredictions] = useState([]);
 
-  // Call backend that fetches users predictions.
-  const fetchPredictions = async (userId) => {
-    const fetchedPredictions = mockData.predictions;
-    const userPredictions = fetchedPredictions.filter(
-      (prediction) => prediction.userId === userId
-    );
-    setPredictions(userPredictions);
-  };
+  const fetchPredictions = async (userId) => {};
 
   useEffect(() => {
-    if (profile) {
-      fetchPredictions(profile.id);
-    } else if (user) {
-      fetchPredictions(user.id);
-    }
+    const userId = profile?.id || user?.id;
+    if (userId) fetchPredictions(userId);
   }, [profile, user]);
 
   if (!user && !profile) {
@@ -43,13 +31,9 @@ function Profile({ profile }) {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col items-center">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full">
           <div className="flex flex-col gap-8 justify-center items-center">
-            {/* Profile Card */}
             <ProfileCard user={displayedProfile} />
-
-            {/* Stats */}
             <ScoreCard user={displayedProfile} />
           </div>
-          {/* Predictions and Badges */}
           <div className="flex justify-center">
             <ContentCard predictions={predictions} />
           </div>
@@ -57,6 +41,6 @@ function Profile({ profile }) {
       </main>
     </div>
   );
-}
+};
 
 export default Profile;
