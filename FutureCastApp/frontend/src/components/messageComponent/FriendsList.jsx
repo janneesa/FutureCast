976c35e0774542c6c0e31dfa0b81da65486c8bd9
ronistr <roni.strom02@gmail.com>
2024-11-28 16:42:24 +0200

    Added users to friendslist in messages

--- FutureCastApp/frontend/src/components/messageComponent/FriendsList.jsx
+++ FutureCastApp/frontend/src/components/messageComponent/FriendsList.jsx
@@ -86,7 +86,20 @@ function FriendsList() {
             </div>
             <h3 className="card-header">Your conversations</h3>
             <div className="card">
-                <ul>
+                <ul className="max-h-[calc(100vh-20rem)] overflow-y-auto">
+                    {friends.map((friend) => (
+                        <li className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-500" key={friend.username}>
+                            <img
+                                src={friend.avatar}
+                                alt="avatar"
+                                className="w-10 h-10 rounded-full"
+                            />
+                            <div className="ml-2">
+                                <h4 className="font-semibold">{friend.username}</h4>
+                                <p className="text-sm">Last message</p>
+                            </div>
+                        </li>
+                    ))}
                     <li className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-500">
                         <img
                             src="https://preview.redd.it/rwl5o5xm2tv71.jpg?width=640&crop=smart&auto=webp&s=ed06c6952948f95b91e09bdc4988ea1e1fab449b"
@@ -106,6 +119,50 @@ function FriendsList() {
                         />
                         <div className="ml-2">
                             <h4 className="font-semibold">Friend 3</h4>
+                            <p className="text-sm">Last message</p>
+                        </div>
+                    </li>
+                    <li className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-500">
+                        <img
+                            src="https://preview.redd.it/rwl5o5xm2tv71.jpg?width=640&crop=smart&auto=webp&s=ed06c6952948f95b91e09bdc4988ea1e1fab449b"
+                            alt="avatar"
+                            className="w-10 h-10 rounded-full"
+                        />
+                        <div className="ml-2">
+                            <h4 className="font-semibold">Friend 4</h4>
+                            <p className="text-sm">Last message</p>
+                        </div>
+                    </li>
+                    <li className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-500">
+                        <img
+                            src="https://preview.redd.it/rwl5o5xm2tv71.jpg?width=640&crop=smart&auto=webp&s=ed06c6952948f95b91e09bdc4988ea1e1fab449b"
+                            alt="avatar"
+                            className="w-10 h-10 rounded-full"
+                        />
+                        <div className="ml-2">
+                            <h4 className="font-semibold">Friend 5</h4>
+                            <p className="text-sm">Last message</p>
+                        </div>
+                    </li>
+                    <li className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-500">
+                        <img
+                            src="https://preview.redd.it/rwl5o5xm2tv71.jpg?width=640&crop=smart&auto=webp&s=ed06c6952948f95b91e09bdc4988ea1e1fab449b"
+                            alt="avatar"
+                            className="w-10 h-10 rounded-full"
+                        />
+                        <div className="ml-2">
+                            <h4 className="font-semibold">Friend 6</h4>
+                            <p className="text-sm">Last message</p>
+                        </div>
+                    </li>
+                    <li className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-500">
+                        <img
+                            src="https://preview.redd.it/rwl5o5xm2tv71.jpg?width=640&crop=smart&auto=webp&s=ed06c6952948f95b91e09bdc4988ea1e1fab449b"
+                            alt="avatar"
+                            className="w-10 h-10 rounded-full"
+                        />
+                        <div className="ml-2">
+                            <h4 className="font-semibold">Friend 7</h4>
                             <p className="text-sm">Last message</p>
                         </div>
                     </li>
